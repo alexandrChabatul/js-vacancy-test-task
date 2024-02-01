1bc760b569392339737dbe58d53ee5a8f109ea32
Alexandr Chabatul <sashkill94@gmail.com>
2024-02-02 00:04:56 +0300

    styles: made home page adaptive

--- apps/api/src/resources/product/actions/list.ts
+++ apps/api/src/resources/product/actions/list.ts
@@ -32,7 +32,7 @@ type ValidatedData = z.infer<typeof schema>;
 
 async function handler(ctx: AppKoaContext<ValidatedData>) {
   const { perPage, page, sort, searchValue, filter } = ctx.validatedData;
-  console.log(perPage, page, sort, searchValue, filter);
+  await new Promise((resolve) => setTimeout(resolve, 50000));
 
   const validatedSearch = searchValue.split('\\').join('\\\\').split('.').join('\\.');
   const regExp = new RegExp(validatedSearch, 'gi');
